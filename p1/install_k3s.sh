e1ce1c323261a48bf0c3c72621f770edb14f69e8
fdeĉ <fd@42l.fr>
2022-10-20 23:21:17 +0200

    fix ssh key copying + fix key removing too early + better comments

--- p1/install_k3s.sh
+++ p1/install_k3s.sh
@@ -4,12 +4,22 @@ if [ "$1" = "controller" ]
 then
 	echo "[provisioned script] Starting installation in controller mode ..."
 	curl -sfL https://get.k3s.io | sh -
-	sleep 30
+	echo "[provisioned script] Waiting for k3s to start ..."
+	while [ ! -f /var/lib/rancher/k3s/server/node-token ]
+	do
+		sleep 1
+	done
+	echo "[provisioned script] Copying node-token to shared folder ..."
 	cp /var/lib/rancher/k3s/server/node-token /vagrant/
+	echo "[provisioned script] Authorizating ssh key ..."
 	cat /vagrant/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 else
 	echo "[provisioned script] Starting installation in agent mode ..."
 	curl -sfL https://get.k3s.io | \
 		K3S_TOKEN=$(cat /vagrant/node-token) K3S_URL=https://$2:6443 sh -
+	echo "[provisioned script] Authorizating ssh key ..."
 	cat /vagrant/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
+
+	echo "[provisioned script] Removing files from shared folder ..."
+	rm /vagrant/id_rsa.pub /vagrant/node-token
 fi
