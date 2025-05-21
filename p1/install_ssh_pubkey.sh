f95e4b9656e099bc801627b478af08a1ebe6c5c7
François Descamps <francois-descamps@protonmail.com>
2025-05-21 14:46:47 +0200

    Remove 42 headers

--- p1/install_ssh_pubkey.sh
+++ p1/install_ssh_pubkey.sh
@@ -1,16 +1,4 @@
 #! /bin/bash
-#
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    install_k3s.sh                                     :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: frdescam <marvin@42.fr>                    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2022/10/17 19:44:43 by frdescam          #+#    #+#              #
-#    Updated: 2022/10/21 19:46:15 by frdescam         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
 
 echo "[install ssh pubkey script] installing ssh key ..."
 cat /vagrant/id_ed25519.pub >> /home/vagrant/.ssh/authorized_keys
