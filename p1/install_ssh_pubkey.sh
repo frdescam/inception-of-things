99a51d6395509ee5f26ac813b3062075d932c8f3
François Descamps <francois-descamps@protonmail.com>
2025-05-21 14:37:57 +0200

    Moving ssh pubkey installation to separated script

--- p1/install_ssh_pubkey.sh
+++ p1/install_ssh_pubkey.sh
@@ -12,32 +12,5 @@
 #                                                                              #
 # **************************************************************************** #
 
-INSTALLATION_MODE="$1"
-INSTALLATION_IP="$2"
-CONTROLLER_IP="$3"
-
-if [ "$INSTALLATION_MODE" = "controller" ]
-then
-	echo "[k3s installation script] Starting installation in controller mode ..."
-	curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--node-ip=$INSTALLATION_IP" sh -
-
-	echo "[k3s installation script] Waiting for k3s to start ..."
-	while [ ! -f /var/lib/rancher/k3s/server/node-token ]
-	do
-		sleep 1
-	done
-
-	echo "[k3s installation script] Copying k3s token to shared folder ..."
-	cp /var/lib/rancher/k3s/server/node-token /vagrant/
-
-else
-	echo "[k3s installation script] Starting installation in agent mode ..."
-	curl -sfL https://get.k3s.io | \
-		K3S_TOKEN="$(cat /vagrant/node-token)" \
-		K3S_URL="https://$CONTROLLER_IP:6443" \
-		INSTALL_K3S_EXEC="--node-ip=$INSTALLATION_IP" \
-		sh -
-
-	echo "[k3s installation script] Removing k3s token from shared folder ..."
-	rm /vagrant/node-token
-fi
+echo "[install ssh pubkey script] installing ssh key ..."
+cat /vagrant/id_ed25519.pub >> /home/vagrant/.ssh/authorized_keys
