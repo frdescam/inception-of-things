592054bf3b8bcab075ac4cb036acab59db2aa223
François Descamps <francois-descamps@protonmail.com>
2025-05-24 21:12:04 +0200

    Cleaned services/app files and cleaning deploy_services to new services names

--- p2/deploy_services.sh
+++ p2/deploy_services.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-# SERVICES_FOLDER="/services"
-
-# for service in app1
-# do
-# 	kubectl apply -f $SERVICES_FOLDER/$service/$service.deployment.yaml
-# 	#kubectl apply -f $SERVICES_FOLDER/$service/$service.service.yaml
-# done
-
-# kubectl apply -f $SERVICES_FOLDER/ingress.yaml
-
 SERVICES_FOLDER="/services"
 
 for service in app1 app2 app3
 do
     echo "Applying Deployment for $service..."
-    kubectl apply -f $SERVICES_FOLDER/$service/$service.deployment.yaml
-    # echo "Applying Service for $service..."
-    # kubectl apply -f $SERVICES_FOLDER/$service/$service.service.yaml
+    kubectl apply -f $SERVICES_FOLDER/$service/$service.yaml
 done
 
 echo "Applying Ingress..."
