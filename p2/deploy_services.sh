6256e87ba08ef27d9dd2d6465b0883ea9621138c
Leonard TOURET <ltouret@f6r2s16.clusters.42paris.fr>
2025-05-24 18:19:45 +0200

    added others pods and services for part two, merged files of pods and services, added ingress logic for others apps

--- p2/deploy_services.sh
+++ p2/deploy_services.sh
@@ -5,8 +5,7 @@ SERVICES_FOLDER="/services"
 for service in app1
 do
 	kubectl apply -f $SERVICES_FOLDER/$service/$service.deployment.yaml
-	kubectl apply -f $SERVICES_FOLDER/$service/$service.service.yaml
+	#kubectl apply -f $SERVICES_FOLDER/$service/$service.service.yaml
 done
 
 kubectl apply -f $SERVICES_FOLDER/ingress.yaml
-
