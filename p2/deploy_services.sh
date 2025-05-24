051bea7dde28c36dcaa28e2b6856ebb101389035
Leonard TOURET <ltouret@f6r2s16.clusters.42paris.fr>
2025-05-24 18:57:17 +0200

    Fixed deploy services script

--- p2/deploy_services.sh
+++ p2/deploy_services.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+# SERVICES_FOLDER="/services"
+
+# for service in app1
+# do
+# 	kubectl apply -f $SERVICES_FOLDER/$service/$service.deployment.yaml
+# 	#kubectl apply -f $SERVICES_FOLDER/$service/$service.service.yaml
+# done
+
+# kubectl apply -f $SERVICES_FOLDER/ingress.yaml
+
 SERVICES_FOLDER="/services"
 
-for service in app1
+for service in app1 app2 app3
 do
-	kubectl apply -f $SERVICES_FOLDER/$service/$service.deployment.yaml
-	#kubectl apply -f $SERVICES_FOLDER/$service/$service.service.yaml
+    echo "Applying Deployment for $service..."
+    kubectl apply -f $SERVICES_FOLDER/$service/$service.deployment.yaml
+    # echo "Applying Service for $service..."
+    # kubectl apply -f $SERVICES_FOLDER/$service/$service.service.yaml
 done
 
+echo "Applying Ingress..."
 kubectl apply -f $SERVICES_FOLDER/ingress.yaml
+
+echo "All resources applied!"
