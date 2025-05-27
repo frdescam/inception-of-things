3f12118f8548fb60934df03acbca3af276f64da8
Francois Descamps <francois-descamps@protonmail.com>
2025-05-27 16:15:08 +0200

    Add http to https redirection in p2

--- p2/scripts/deploy_services.sh
+++ p2/scripts/deploy_services.sh
@@ -4,11 +4,17 @@ SERVICES_FOLDER="/services"
 
 for service in app1 app2 app3
 do
-    echo "Applying Deployment for $service..."
+    echo "[services deployment script] Deploying $service..."
     kubectl apply -f $SERVICES_FOLDER/$service/$service.yaml
 done
 
-echo "Applying Ingress..."
+echo "[services deployment script] Waiting for traefik CRD to be installed ..."
+while ! kubectl api-resources | grep traefik.io/v1alpha1 | grep "Middleware\b" > /dev/null 2>&1
+do
+    sleep 2
+done
+
+echo "[services deployment script] Deploying Ingress..."
 kubectl apply -f $SERVICES_FOLDER/ingress.yaml
 
-echo "All resources applied!"
+echo "[services deployment script] All resources deployed!"
