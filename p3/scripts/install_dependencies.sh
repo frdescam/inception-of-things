ad1597ff9a24b894245afe90b4158e30a2c4d739
Super Leo VM Much wow <naaaaaah@protonmail.com>
2025-05-27 22:00:03 +0200

    Added docker to install dependecies, added sleep that wait for argocd to be ready to deploy, removed TODOs and enabled port fowarding for argocd web ui.

---
+++ p3/scripts/install_dependencies.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Installing docker"
+apt-get update
+apt-get install ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "Installing kubectl in /usr/local/bin"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+mv ./kubectl /usr/local/bin/kubectl
+
+echo "Installing K3d"
+wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
