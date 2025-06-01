b2413dceeba30d19dcae1b823dd6eaa7afda6bbf
Super Leo VM Much wow <jiloh55@gmail.com>
2025-06-01 16:38:30 +0200

    Fixed some errors in gitlab install (bonus).

--- bonus/gitlab/deploy_gitlab.sh
+++ bonus/gitlab/deploy_gitlab.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# do we change the domain name?
-
+echo "Deleting cluster if it already exists and creating it"
 sudo k3d cluster delete gitlab
-echo "Deploying gitlab"
 sudo k3d cluster create gitlab --port "80:80@loadbalancer" --port "443:443@loadbalancer" --port "22:22@server:0" 
 sudo kubectl create namespace gitlab
-#sudo kubectl create secret generic -n gitlab gitlab-gitlab-initial-root-password --from-literal=password=iotiot
-sudo helm upgrade --install gitlab gitlab/gitlab   --timeout 600s   --set global.hosts.domain=iot.com   --set global.hosts.externalIP=127.0.0.1  --set certmanager-issuer.email=me@example.com -n gitlab
-sleep 20 # change this
+echo "Deploying gitlab"
+sudo helm repo add gitlab https://charts.gitlab.io/
+sudo helm repo update
+sudo helm upgrade --install gitlab gitlab/gitlab --timeout 600s --set global.hosts.domain=iot.local --set global.hosts.externalIP=127.0.0.1 --set certmanager-issuer.email=me@example.com -n gitlab
+echo "Deleting Traefik in kube-system"
 sudo kubectl delete service traefik -n kube-system
-#sudo kubectl create secret generic -n gitlab gitlab-gitlab-initial-root-password --from-literal=password=iotiotiot
+sudo kubectl delete deployment traefik -n kube-system
+echo "Gitlab login: root"
+echo "Gitlab Password:"
 sudo kubectl get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' -n gitlab | base64 --decode ; echo
