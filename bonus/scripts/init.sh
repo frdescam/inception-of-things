448281c3102c79e4789514a148438b5f668999c1
ltouret <jiloh55@gmail.com>
2025-06-04 12:58:36 +0200

    Fixed bonus part 3 scripts

--- bonus/scripts/init.sh
+++ bonus/scripts/init.sh
@@ -2,8 +2,8 @@
 
 CONF_FOLDER="/home/iot/inception-of-things/p3/scripts"
 
-bash $CONF_FOLDER/deploy_argocd.sh
-bash $CONF_FOLDER/deploy_part3.sh
+sudo bash $CONF_FOLDER/deploy_argocd.sh
+sudo bash $CONF_FOLDER/deploy_part3.sh
 
 echo "Port fowarding for argocd Web"
-kubectl port-forward svc/argocd-server -n argocd 8080:443
+sudo kubectl port-forward svc/argocd-server -n argocd 8080:443
