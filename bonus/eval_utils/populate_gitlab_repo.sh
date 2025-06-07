077e15c704af7f94610a8e2a9607ae602a89b7e4
iot <iot@localhost.localdomain>
2025-06-07 17:21:27 +0200

    Fix populate gitlab repo script

--- bonus/eval_utils/populate_gitlab_repo.sh
+++ bonus/eval_utils/populate_gitlab_repo.sh
@@ -9,9 +9,14 @@ NEW_REMOTE_URL="git@gitlab.iot.local:root/ltouret-argocd.git"
 BRANCH_TO_PUSH="main"
 
 echo "Add this ssh key to gitlab.."
-ssh-keygen -t ed25519 -C "bestvmever@iot.local"
+rm -f ~/.ssh/id_ed25519.pub ~/.ssh/id_ed25519
+ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N '' -C "bestvmever@iot.local"
+echo -e "\e[41m\e[5mPlease copy this ssh key in the Gitlab UI at gitlab.iot.local :\e[0m"
 cat ~/.ssh/id_ed25519.pub
 
+read -p "Type 'y' and enter to proceed: " confirmation
+
+echo -e "\e[41m\e[5mAre you sure ?\e[0m"
 read -p "Type 'y' and enter to proceed: " confirmation
 
 if [ "$confirmation" != "y" ]
@@ -24,15 +29,11 @@ echo "Starting Git operations..."
 git clone "$CLONE_REPO_URL" "$LOCAL_REPO_DIR"
 cd "$LOCAL_REPO_DIR" || cd "."
 
-echo "Adding or updating new remote: $NEW_REMOTE_NAME with URL $NEW_REMOTE_URL..."
-git remote add "$NEW_REMOTE_NAME" "$NEW_REMOTE_URL" || git remote set-url "$NEW_REMOTE_NAME" "$NEW_REMOTE_URL"
-
-echo "Pulling latest changes from existing 'origin' remote (if applicable)..."
-git pull origin "$BRANCH_TO_PUSH"
+echo "Adding new remote: $NEW_REMOTE_NAME with URL $NEW_REMOTE_URL..."
+git remote add "$NEW_REMOTE_NAME" "$NEW_REMOTE_URL"
 
 echo "Pushing $BRANCH_TO_PUSH branch to the new remote '$NEW_REMOTE_NAME'..."
+ssh-keyscan gitlab.iot.local >> ~/.ssh/known_hosts
 git push -u "$NEW_REMOTE_NAME" "$BRANCH_TO_PUSH"
 
-echo "Git operations completed."
-echo "Current remotes:"
-git remote -v
+echo "Gitlab repo was successfully populated."
